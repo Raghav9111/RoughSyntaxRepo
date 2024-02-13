9a8af4bbcbac937206ce53d9d2c7a9a2b4480c66
HP <Raghav>
2024-02-13 15:50:11 +0530

    Inital

---
+++ SyntaxSavants/src/main/java/com/syntaxSavants/SyntaxSavantsApplication.java
@@ -0,0 +1,22 @@
+package com.syntaxSavants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+
+@EnableAutoConfiguration
+public class SyntaxSavantsApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyntaxSavantsApplication.class, args);
+	}
+	 @Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	  return application.sources(SyntaxSavantsApplication.class);
+	 }
+}
---
+++ SyntaxSavants/src/test/java/com/syntaxSavants/SyntaxSavantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syntaxSavants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyntaxSavantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
